ae810decf10bf86fa94219d3d74384d8d0ab0333
tuffo19 <giacomo.tufano@gmail.com>
2025-02-06 23:12:39 +0100

    rimossi i file da non versionare

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-d
+import { Telegraf } from "telegraf";
+
+const bot = new Telegraf("<INSERITE IL VOSTRO TOKE QUI>");
+
+bot.on("message", (ctx) => {
+  ctx.reply("Ciao, sono un bot molto stupido!");
+});
+
+bot.launch();
